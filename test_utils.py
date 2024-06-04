6a5561b7ba56547c331b38769d4fc3b73903365e
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-06-04 17:39:00 +0300

    Added call_write_to_file

--- test_utils.py
+++ test_utils.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from KattisToGithub.src.solved_problem import SolvedProblem, ProblemStatus
 
 
@@ -6,3 +7,6 @@ def get_solved_problem() -> SolvedProblem:
 
 def get_problem_status(val: int) -> ProblemStatus:
     return ProblemStatus(val)
+
+def call_write_to_file(sp: SolvedProblem, directory: str) -> None:
+    sp.write_to_file(Path(directory))
